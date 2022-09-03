cbfdb8e2483cccad0e90d93ac18097f02ea7214e
VitalikBaiduk <vitalilbaiduj@icloud.com>
2022-09-03 15:56:12 +0300

    add todolist

---
+++ src/Todolist.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+type TodolistProps = {
+  title: string;
+  arrData: Array<any>;
+};
+
+export const Todolist: React.FC<TodolistProps> = ({ title, arrData }) => {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <div>
+        <input />
+        <button>+</button>
+      </div>
+      <ul>
+        {arrData.map((item: any) => {
+          return (
+            <li>
+              <input type="checkbox" checked={item.isDone} />
+              <span>{item.title}</span>
+            </li>
+          );
+        })}
+      </ul>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+    </div>
+  );
+};
